f84dbf405e0d140e578743ad415615cc06a68082
Daniel Stutzman <dtstutz@gmail.com>
2017-08-04 11:24:14 -0600

    Start parsing Kotlin code

---
+++ parser/kotlin_demo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+//go:generate $GOPATH/bin/pigeon -o kotlin.peg.go kotlin.peg
+
+func main() {
+	input := "fun hello"
+	parsed, err := Parse("", []byte(input))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", parsed)
+}
