e1958a98d455467d2bcdeb375d10165127f5d57d
Daniel Stutzman <dtstutz@gmail.com>
2017-08-04 11:30:35 -0600

    Can parse one argument

--- parser/kotlin_demo.go
+++ parser/kotlin_demo.go
@@ -7,7 +7,7 @@ import (
 //go:generate $GOPATH/bin/pigeon -o kotlin.peg.go kotlin.peg
 
 func main() {
-	input := "fun hello"
+	input := "fun hello(args: Array<String>)"
 	parsed, err := Parse("", []byte(input))
 	if err != nil {
 		panic(err)
